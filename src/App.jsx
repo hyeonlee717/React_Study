9c7ccb8692657eb3b3a37364f9ab0215290f605a
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2025-12-10 21:41:09 +0900

    component

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,18 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
 
+      <Modal></Modal>
+
+    </div>
+  )
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   )
 }
